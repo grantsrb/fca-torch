5cd5eb8e128a5eeb4a8d839385ac12e13b4eaeb7
Satchel Grant <grantsrb@stanford.edu>
2025-03-09 23:35:33 -0700

    move defaults, more printing

--- chain_main.py
+++ chain_main.py
@@ -15,6 +15,7 @@ from utils import join_configs, get_command_line_args
 from dl_utils.save_io import (
     get_save_folder, load_checkpoint, get_checkpoints, get_folder_from_path
 )
+from constants import CHAIN_DEFAULTS
 import torch
 import numpy as np
 import pandas as pd
@@ -22,43 +23,9 @@ import os
 import time
 import copy
 
-defaults = {
-    "save_root": "/data2/grantsrb/fca_saves/",
-    "exp_name": "fca_prototyping",
-    'task_params': {
-        'n_pairs': 4,
-        'n_samples': 1000,
-    },
-    'model_params': {
-        'embedding_dim': 32,
-        'd_model': 512,
-        'n_layers': 3,
-        'nonlinearity': "ReLU",
-        'lnorm': True,
-    },
-    'lr': 0.001,
-    'num_epochs': 100000,
-    'batch_size': 128,
-    "patience": 100,
-    "plateau": 0.005,
-    'model_load_path': "/data2/grantsrb/fca_saves/fca_prototyping/fca_prototyping_12/",
-    "save_to_load_path": True,
-
-    'fca_load_path': None,
-    'fca_params': {
-        "max_rank": None
-    },
-    'fca_layers': ["hidden_layers.1"],
-    "fca_acc_threshold": 0.99, # stops early when fca module reaches this trn and val acc
-    "ensure_ortho_chain": False, # will load the sd from the previous fca into the newest 
-
-    'persistent_keys': [
-        'fca_params', 'fca_layers', 'lr',
-        "fca_load_path", "batch_size", "num_epochs",
-    ],
-}
 
 if __name__ == "__main__":
+    defaults = copy.deepcopy(CHAIN_DEFAULTS)
     arg_config = get_command_line_args()
     config = join_configs(kwargs=arg_config, defaults=defaults)
     if not (config.get("model_load_path", None) and config.get("save_to_load_path", False)):
@@ -72,7 +39,7 @@ if __name__ == "__main__":
     config["seed"] = config.get("seed", 123456)
     torch.manual_seed(config["seed"])
     np.random.seed(config["seed"])
-    
+
     # track acc of last fca and number of fcas compared to model size
     data_dict = {
         "chain_num": [],
@@ -85,6 +52,7 @@ if __name__ == "__main__":
     }
     assert not config["fca_load_path"] and len(config["fca_layers"])==1
     if "full_rank" not in config:
+        print("Path:", config['model_load_path'])
         checkpoint = load_checkpoint(config['model_load_path'])
         layer = config["fca_layers"][0]
         sd = checkpoint["model_state_dict"]
@@ -105,22 +73,25 @@ if __name__ == "__main__":
         print("New Chain", chain_num,
             "-- Cumu Rank:", cumu_rank,
             "-- Full Rank", full_rank)
+        if config["ensure_ortho_chain"]:
+            print("Ensuring orthogonality")
+        else:
+            print("Allowing deviations from orthogonality")
         time.sleep(2)
         startt = time.time()
+
         rank = full_rank - cumu_rank
         config = copy.deepcopy(og_config)
         config["fca_params"]["max_rank"] = rank
         config["fca_load_path"] = metrics.get("save_path", None)
 
         metrics = train(config)
+        if metrics is None:
+            break
 
         fca_rank = list(metrics["ranks"].values())[-1] # list due to layers
-        if config["ensure_ortho_chain"]:
-            new_rank = fca_rank-cumu_rank
-            cumu_rank = fca_rank
-        else:
-            new_rank = fca_rank
-            cumu_rank += fca_rank
+        new_rank = fca_rank
+        cumu_rank += fca_rank
         data_dict["chain_num"].append(chain_num)
         data_dict["trn_acc"].append(metrics["trn_acc"])
         data_dict["trn_loss"].append(metrics["trn_loss"])
@@ -129,14 +100,17 @@ if __name__ == "__main__":
         data_dict["rank"].append(new_rank)
         data_dict["cumu_rank"].append(cumu_rank)
         print("Exec Time:", time.time()-startt)
+
     save_path = os.path.join(
         get_folder_from_path(config["model_load_path"]),
         "chain_results.csv",
     )
     df = pd.DataFrame(data_dict)
-    df.to_csv(save_path, header=True, index=False)
     print("DataFrame:")
     print(df)
-    print("Saving to", save_path)
-    print("Finished Chain")
+    print("Model Path:", config["model_load_path"])
+    if len(df)>0:
+        print("Saving to", save_path)
+        df.to_csv(save_path, header=True, index=False)
+    print("Finished FCA Chain")
     print("----")
